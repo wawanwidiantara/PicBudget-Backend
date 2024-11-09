5938aefdadb045ad36cd91913a2117eab1941d12
wawanwidiantara <gdwidi13@gmail.com>
2024-11-09 21:48:44 +0700

    feat: Add Payment model and update Membership model with type and status changes

--- picbudget/memberships/admin.py
+++ picbudget/memberships/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models.membership import Membership
+from .models.payment import Payment
 
 admin.site.register(Membership)
+admin.site.register(Payment)
--- picbudget/memberships/models/membership.py
+++ picbudget/memberships/models/membership.py
@@ -4,17 +4,21 @@ from uuid import uuid4
 
 # Create your models here.
 class Membership(models.Model):
+    MEMBERSHIP_TYPE = (
+        ("monthly", "Monthly"),
+        ("yearly", "Yearly"),
+    )
     MEMBERSHIP_STATUS = (
         ("active", "Active"),
-        ("pending", "Pending"),
         ("inactive", "Inactive"),
     )
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField()
+    type = models.CharField(max_length=10, choices=MEMBERSHIP_TYPE, default="monthly")
     status = models.CharField(
-        max_length=10, choices=MEMBERSHIP_STATUS, default="pending"
+        max_length=10, choices=MEMBERSHIP_STATUS, default="inactive"
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
