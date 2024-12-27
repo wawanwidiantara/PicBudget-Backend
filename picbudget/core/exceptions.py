62e39ea68e6b5605190a26b1f45241f54cd7afa6
wawanwidiantara <gdwidi13@gmail.com>
2024-12-27 22:19:34 +0700

    feat: Update custom exception handler to return full error details in API responses

--- picbudget/core/exceptions.py
+++ picbudget/core/exceptions.py
@@ -2,12 +2,14 @@ from rest_framework.views import exception_handler
 from rest_framework.exceptions import APIException
 
 
-def full_details_exception_handler(exc, context):
+def custom_exception_handler(exc, context):
 
     response = exception_handler(exc, context)
 
     if isinstance(exc, APIException) and response is not None:
 
-        response.data = {"errors": response.data}
+        full_details = exc.get_full_details()
+
+        response.data = {"errors": full_details}
 
     return response
